9859d5ab54dfb01ba1fb9fa31b8f2a6ad33bf18f
Norapath Arjanurak <norapath.arj@gmail.com>
2024-11-15 16:23:40 +0700

    fea: update finish line

--- hand-gesture-detection-mediapipe/main.py
+++ hand-gesture-detection-mediapipe/main.py
@@ -1,6 +1,6 @@
 # Main game loop
 board = [" " for _ in range(9)]
-from UR3e_control import robot_move, human_move, play_position, home, UR_set_up, test, grid
+from UR3e_control import robot_move, human_move, play_position, home, UR_set_up, test, grid, gripper_connection,gripper_open,gripper_close, draw_end_line
 from minimax_tictactoe import display_board, check_winner, is_board_full, board, computer_move
 from pymodbus.client import ModbusTcpClient
 
@@ -14,6 +14,12 @@ def server_connection():
     client_socket.connect(('localhost', 12345))
     print("Connected to the server.")
 
+def gripper_init():
+    gripper_connection()
+    gripper_open()
+    time.sleep(3)
+    gripper_close()
+
 def play_game():
     print("Welcome to Tic-Tac-Toe!")
     print("The board positions are as follows:")
@@ -22,15 +28,15 @@ def play_game():
     display_board()
     while True:
         # Player's turn
-        command = client_socket.recv(1024).decode() # Receive command from server(Flush)
+        # command = client_socket.recv(1024).decode() # Receive command from server(Flush)
         while True:
             try:
-                command = client_socket.recv(1024).decode()
-                while not(command[-1].isdigit()):
-                    command = client_socket.recv(1024).decode()
-                print(f"Received Command: {command[-1]}")
-                user_pos = int(command[-1]) - 1
-                # user_pos = int(input("Choose your position (1-9): ")) - 1
+                # command = client_socket.recv(1024).decode()
+                # while not(command[-1].isdigit()):
+                #     command = client_socket.recv(1024).decode()
+                # print(f"Received Command: {command[-1]}")
+                # user_pos = int(command[-1]) - 1
+                user_pos = int(input("Choose your position (1-9): ")) - 1
                 if user_pos in range(9) and board[user_pos] == " ":
                     board[user_pos] = "X"
                     break
@@ -46,12 +52,15 @@ def play_game():
         
         # Check if player wins
         if check_winner("X"):
+            draw_end_line("X")
             print("Congratulations! You win!")
             break
         
         # Check if it's a tie
         if is_board_full():
             print("It's a tie!")
+            play_position()
+            home()
             break
         
         # Computer's turn
@@ -61,22 +70,25 @@ def play_game():
         
         # Check if computer wins
         if check_winner("O"):
+            draw_end_line("O")
             print("Computer wins! Better luck next time!")
             break
         
         # Check if it's a tie
         if is_board_full():
             print("It's a tie!")
+            play_position()
+            home()
             break
 
 
 if __name__ == '__main__':
-        server_connection()
+        # server_connection()
         UR_set_up()
+        # gripper_init()
         home()
         # grid()
         # test()
         # play_position()
-        play_game()
+        # play_game()
         # robot_move(1, 'X')
-        
